45cad540ada2306f1cf1df96a1870c7ea598909e
Marco Barroso <alu0101386560@ull.edu.es>
2023-11-14 21:24:18 +0000

    continuacion restaurants-models, falta la validación

--- server/src/restaurants/restaurants-models.ts
+++ server/src/restaurants/restaurants-models.ts
@@ -25,3 +25,91 @@ interface restaurantsDocumentInterface {
   // nextReservations: [reservation];
   // historicReservations: [reservation];
 }
+
+
+const RestaurantSchema = new Schema<restaurantsDocumentInterface>({
+  userName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  passwd: {
+    type: String,
+    minlength: 8,
+    required: true,
+    match: [ /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/, 'Password must contain at least one lowercase letter, one uppercase letter and one number' ],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: [ /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Invalid email address' ]
+  },
+  restaurantName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  restaurantAddress: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  timeTable: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+    match: [ /^\d{9}$/, 'Invalid phone number' ]
+  },
+  pictures: {
+    type: [Buffer],
+    required: false,
+    trim: true
+  },
+  menu: {
+    type: Buffer,
+    required: false,
+    trim: true
+  }
+  // ,
+  // availability: { // cambiar interior
+  //   type: String,
+  //   required: true,
+  //   trim: true
+  // },
+  // nextReservations: { // cambiar interior
+  //   type: String,
+  //   required: true,
+  //   trim: true
+  // },
+  // historicReservations: { // cambiar interior
+  //   type: String,
+  //   required: true,
+  //   trim: true
+  // }
+
+});
+
+export const RestaurantModel = model<restaurantsDocumentInterface>('Restaurant', RestaurantSchema);
+
+// export async function validateRestaurantSchema(restaurant: restaurantsDocumentInterface): Promise<{ code: number, errors: string }> {
+  // const existingRestaurant = await RestaurantModel.findOne({
+// }
