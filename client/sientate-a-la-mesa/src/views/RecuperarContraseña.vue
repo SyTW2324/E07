f17dfc5aecb3ca76ef934cb0e789773da863e9ee
Alberto Zarza Martín <alu0101412993@ull.edu.es>
2023-11-14 22:11:39 +0000

    creadas pantallas LOGIN, RECUPERAR CONTRASEÑA + avances en las mismas

---
+++ client/sientate-a-la-mesa/src/views/RecuperarContraseña.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Barnav from '../components/Barnav.vue';
+import Footer from '../components/Footer.vue'
+</script>
+
+<template>
+
+<v-app>
+  <Barnav></Barnav>
+  <v-main
+      class="d-flex align-center justify-center"
+      style="min-height: 300px" >
+
+      
+
+      <v-text-field label="Label"></v-text-field>
+
+
+
+  </v-main>
+
+  <Footer></Footer>
+  
+</v-app>
+
+</template>
