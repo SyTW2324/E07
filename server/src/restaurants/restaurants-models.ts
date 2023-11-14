e573ff5b1d5eaff008d7839820d03da45a13adcc
Marco Barroso <alu0101386560@ull.edu.es>
2023-11-14 21:11:34 +0000

    inicio restaurants-models

---
+++ server/src/restaurants/restaurants-models.ts
@@ -0,0 +1,27 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ */
+
+import {Document, Schema, model} from 'mongoose';
+
+interface restaurantsDocumentInterface {
+  userName: string;
+  passwd: string;
+  email: string;
+
+  restaurantName: string;
+  restaurantAddress: string;
+  description:string;
+  timeTable: string;
+  category: string;
+
+  phoneNumber: string;
+  pictures: [Buffer | null];
+
+  menu: Buffer | null;
+  // availability: [available];
+  // nextReservations: [reservation];
+  // historicReservations: [reservation];
+}
