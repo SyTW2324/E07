e124cfa1786e5938d5b11cf7f13fcd60906ff3d1
Marco Barroso <alu0101386560@ull.edu.es>
2023-11-15 20:03:04 +0000

    comienzo de availability

--- server/src/availability.ts
+++ server/src/availability.ts
@@ -7,7 +7,7 @@
 
 // Con este tipo, un restaurante puede introducir un periodo de tiempo y una franja horario para cada día.
 // Por ejemplo:
-// 01/01/2024 hasta 31/06/2024 de 12:30 a 15:30 se permiten reservas, dentro del horario del restaurante.
+// 01/01/2024 hasta 31/06/2024 de 12:30 a 15:30 se permiten reservas, disponibilidad para 100 personas, dentro del horario del restaurante.
 
 export type availability = {
   // día de inicio
