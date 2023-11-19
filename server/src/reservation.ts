ab597bbb1a42b999d6d9c373cee85790abff2049
Marco Barroso <alu0101386560@ull.edu.es>
2023-11-19 11:26:47 +0000

    pequeños cambios en resevation

--- server/src/reservation.ts
+++ server/src/reservation.ts
@@ -4,13 +4,11 @@
  * Grado en Ingeniería Informática
  */
 
-import { Availability } from "./availability.js";
+import { Available } from "./available.js";
 
 export type Reservation = {
   restaurant: string; // Nombre del establecimiento
   clientUserName: string; // Nombre de usuario del cliente
   numberOfPeople: number; // Número de personas para la reserva
   date: Date; // Día y hora de la reserva
-  availability: Availability; // Información de disponibilidad para la reserva
-  // Otros campos relacionados con la reserva según tus necesidades
 };
