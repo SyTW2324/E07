0041eaaf20b7c04a8c46d07ca6f330acc8bcf554
Marco Barroso <alu0101386560@ull.edu.es>
2023-11-27 21:52:37 +0000

    cambios en disponibilidad, ahora solo se tiene tiempo de cada franja y capacidad de personas por franja

--- server/src/available.ts
+++ server/src/available.ts
@@ -10,14 +10,7 @@
 // 01/01/2024 hasta 31/06/2024 de 12:30 a 15:30 se permiten reservas, disponibilidad para 100 personas, dentro del horario del restaurante.
 
 export type Available = {
-  start: Date;
-  finish: Date;
-  startingHour: string;
-  finishingHour: string;
+  timePeriod: Number; // minutos para cada reserva, por ejemplo 60 minutos.
   numberOfPeople: number;
 };
 
-export function validateAvailable(avail: Available): boolean {
-  // Verificar si la fecha de inicio es anterior a la fecha de finalización
-  return avail.start < avail.finish;
-}
