4a811436b5757dcee50bf3a6eca77a7df7bdcc4b
Ismael Martín Herrera <alu0101397375@ull.edu.es>
2023-12-15 15:47:14 +0000

    Update env-variables.ts

--- server/src/env-variables.ts
+++ server/src/env-variables.ts
@@ -8,7 +8,7 @@ export const uri_bbdd_test: string = 'mongodb+srv://adminProyecto:XrGAiCeGbROwls
 
 export const uri_bbdd_local: string = 'mongodb://adminProyecto:1234@172.16.131.2:27017/sientateMesa?retryWrites=true&w=majority';
 
-export const port: number = 8081;
+export const port: number = 3000;
 
 export const ip_express_despliegue: string = '10.6.128.143';
 
