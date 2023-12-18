5671b4182748a687d6e1a473a20847cffc834de6
imh <alu0101397375@ull.edu.es>
2023-12-18 18:29:27 +0000

    cambio para una nueva collección en la bbdd sólo para desarrollo, aparte de las de test y la de deploy

--- server/src/database/mongoose.ts
+++ server/src/database/mongoose.ts
@@ -2,13 +2,14 @@ import { connect } from 'mongoose';
 import { uri_bbdd } from '../env-variables.js';
 import { uri_bbdd_test } from '../env-variables.js';
 import { uri_bbdd_local } from '../env-variables.js';
+import { uri_bbdd_dev } from '../env-variables.js';
 
 let bbdd = uri_bbdd_local;
 if (process.argv[2] === 'test') {
   console.log('Usando base de datos de test')
   bbdd = uri_bbdd_test;
 } else if (process.argv[2] === 'dev') {
-  bbdd = uri_bbdd;
+  bbdd = uri_bbdd_dev;
 } else if (process.argv[2] === 'deploy') {
   bbdd = uri_bbdd;
 }
