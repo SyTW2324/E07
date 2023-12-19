a4efffdc3392eeba19e2563e30af456c11fa6cfc
imh <alu0101397375@ull.edu.es>
2023-12-19 09:31:26 +0000

    Comentada toda la clase restaurant

--- server/src/models/restaurants/restaurants.ts
+++ server/src/models/restaurants/restaurants.ts
@@ -4,121 +4,121 @@
  * Grado en Ingeniería Informática
  */
 
-import * as mongodb from 'mongodb';
-import { Available } from '../../available.js';
-import { Reservation } from '../../reservation.js';
-import { Timetable } from '../../timeTable.js';
+// import * as mongodb from 'mongodb';
+// import { Available } from '../../available.js';
+// import { Reservation } from '../../reservation.js';
+// import { Timetable } from '../../timeTable.js';
 
 
-export interface restaurantInterface {
-  userName: string;
-  passwd: string;
-  email: string;
+// export interface restaurantInterface {
+//   userName: string;
+//   passwd: string;
+//   email: string;
 
-  _id?: mongodb.ObjectId;
-  restaurantName: string;
-  restaurantAddress: string;
-  description:string;
-  timeTable: Timetable;
-  category: string;
+//   _id?: mongodb.ObjectId;
+//   restaurantName: string;
+//   restaurantAddress: string;
+//   description:string;
+//   timeTable: Timetable;
+//   category: string;
 
-  phoneNumber: string;
-  profilePicture: (Buffer | null);
-  pictures: (Buffer | null)[]; // Array of pictures
+//   phoneNumber: string;
+//   profilePicture: (Buffer | null);
+//   pictures: (Buffer | null)[]; // Array of pictures
 
-  menu: Buffer | null;
-  availability: Available[];
-  nextReservations: Reservation[];
-  historicReservations: Reservation[];
-}
+//   menu: Buffer | null;
+//   availability: Available[];
+//   nextReservations: Reservation[];
+//   historicReservations: Reservation[];
+// }
 
 
 
-export class restaurantClass {
-  constructor(
-    private userName: string,
-    private passwd: string,
-    private email: string,
+// export class restaurantClass {
+//   constructor(
+//     private userName: string,
+//     private passwd: string,
+//     private email: string,
 
-    private restaurantName: string,
-    private restaurantAddress: string,
-    private description:string,
-    private timeTable: Timetable,
-    private category: string,
+//     private restaurantName: string,
+//     private restaurantAddress: string,
+//     private description:string,
+//     private timeTable: Timetable,
+//     private category: string,
 
-    private phoneNumber: string,
-    private profilePicture: Buffer | null,
-    private pictures: (Buffer | null)[],
+//     private phoneNumber: string,
+//     private profilePicture: Buffer | null,
+//     private pictures: (Buffer | null)[],
     
-    private menu: Buffer | null,
-    private availability: Available[],
-    private nextReservations: Reservation[],
-    private historicReservations: Reservation[],
-    private _id?: mongodb.ObjectId
-  ) {}
+//     private menu: Buffer | null,
+//     private availability: Available[],
+//     private nextReservations: Reservation[],
+//     private historicReservations: Reservation[],
+//     private _id?: mongodb.ObjectId
+//   ) {}
 
-  getUserName() {
-    return this.userName;
-  }
+//   getUserName() {
+//     return this.userName;
+//   }
 
-  getPasswd() {
-    return this.passwd;
-  }
+//   getPasswd() {
+//     return this.passwd;
+//   }
 
-  getEmail() {
-    return this.email;
-  }
+//   getEmail() {
+//     return this.email;
+//   }
 
-  getId() {
-    return this._id;
-  }
+//   getId() {
+//     return this._id;
+//   }
 
-  getRestaurantName() {
-    return this.restaurantName;
-  }
+//   getRestaurantName() {
+//     return this.restaurantName;
+//   }
 
-  getRestaurantAddress() {
-    return this.restaurantAddress;
-  }
+//   getRestaurantAddress() {
+//     return this.restaurantAddress;
+//   }
 
-  getDescription() {
-    return this.description;
-  }
+//   getDescription() {
+//     return this.description;
+//   }
 
-  getTimeTable() {
-    return this.timeTable;
-  }
+//   getTimeTable() {
+//     return this.timeTable;
+//   }
 
-  getCategory() {
-    return this.category;
-  }
+//   getCategory() {
+//     return this.category;
+//   }
   
-  getPhoneNumber() {
-    return this.phoneNumber;
-  }
+//   getPhoneNumber() {
+//     return this.phoneNumber;
+//   }
 
-  getProfilePicture() {
-    return this.profilePicture;
-  }
+//   getProfilePicture() {
+//     return this.profilePicture;
+//   }
 
-  getPictures() {
-    return this.pictures;
-  }
+//   getPictures() {
+//     return this.pictures;
+//   }
 
-  getMenu() {
-    return this.menu;
-  }
+//   getMenu() {
+//     return this.menu;
+//   }
 
-  getAvailability() {
-    return this.availability;
-  }
+//   getAvailability() {
+//     return this.availability;
+//   }
 
-  getNextReservations() {
-    return this.nextReservations;
-  }
+//   getNextReservations() {
+//     return this.nextReservations;
+//   }
 
-  getHistoricReservations() {
-    return this.historicReservations;
-  }
+//   getHistoricReservations() {
+//     return this.historicReservations;
+//   }
 
-}
+// }
