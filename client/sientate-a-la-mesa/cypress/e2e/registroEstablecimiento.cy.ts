ce02cd4638cd780ee9c2272e206c1c97d49703db
Ismael Martín Herrera <alu0101397375@ull.edu.es>
2023-12-29 10:50:47 +0000

    comentados test de crypress

--- client/sientate-a-la-mesa/cypress/e2e/registroEstablecimiento.cy.ts
+++ client/sientate-a-la-mesa/cypress/e2e/registroEstablecimiento.cy.ts
@@ -1,61 +1,61 @@
-import axios from 'axios';
-import { baseUrl } from '../../src/env/env-variables';
+// import axios from 'axios';
+// import { baseUrl } from '../../src/env/env-variables';
 
-describe('Registro de establecimiento', () => {
-  beforeEach(async () => {
+// describe('Registro de establecimiento', () => {
+//   beforeEach(async () => {
     
-    const userDelete = {
-      userName: "establecimientoPrueba",
-    }
+//     const userDelete = {
+//       userName: "establecimientoPrueba",
+//     }
     
-    const response = await axios.delete(`${baseUrl}restaurants?userName=${userDelete.userName}`);    
+//     const response = await axios.delete(`${baseUrl}restaurants?userName=${userDelete.userName}`);    
     
-  })
+//   })
 
     
 
-  it('Registro de establecimiento', () => {
-    cy.visit('http://localhost:5173')
+//   it('Registro de establecimiento', () => {
+//     cy.visit('http://localhost:5173')
 
-    // hace click en boton de login
-    cy.get('#botonRegistro').click()
-    cy.get('#botonRegistroEstablecimiento').click()
+//     // hace click en boton de login
+//     cy.get('#botonRegistro').click()
+//     cy.get('#botonRegistroEstablecimiento').click()
 
-    cy.get('#restaurantname').type('Establecimiento de prueba')
-    cy.get('#address').type('Calle falsa 123')
-    cy.get('#description').type('Establecimiento de prueba, nueva descripcion')
+//     cy.get('#restaurantname').type('Establecimiento de prueba')
+//     cy.get('#address').type('Calle falsa 123')
+//     cy.get('#description').type('Establecimiento de prueba, nueva descripcion')
     
-    // ? checklist pendiente
-    // Suponiendo que los días de la semana son: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
-    const selectedDays = ['Lunes', 'Miércoles', 'Viernes'];
-    // Iterar sobre los días de la semana y marcarlos en el formulario
-    selectedDays.forEach(day => {
-    cy.contains(day).click();
-    });
+//     // ? checklist pendiente
+//     // Suponiendo que los días de la semana son: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
+//     const selectedDays = ['Lunes', 'Miércoles', 'Viernes'];
+//     // Iterar sobre los días de la semana y marcarlos en el formulario
+//     selectedDays.forEach(day => {
+//     cy.contains(day).click();
+//     });
 
-    // ? hora inicio
-    cy.get('#horaInicio').type('09:00');
-    // ? hora fin
-    cy.get('#horaFin').type('18:00');
+//     // ? hora inicio
+//     cy.get('#horaInicio').type('09:00');
+//     // ? hora fin
+//     cy.get('#horaFin').type('18:00');
     
-    // ? Categoría
-    // cy.get('#category').scrollTo()
-        // Encuentra el v-select y haz clic en él para abrir el menú
-    cy.get('.v-select').click();
+//     // ? Categoría
+//     // cy.get('#category').scrollTo()
+//         // Encuentra el v-select y haz clic en él para abrir el menú
+//     cy.get('.v-select').click();
 
-    cy.contains('pizzeria').click()  // Seleccionar la categoría deseada
+//     cy.contains('pizzeria').click()  // Seleccionar la categoría deseada
 
-    cy.get('#email').type('establecimientoPrueba@gmail.com')
-    cy.get('#phone').type('666666666')
-    cy.get('#username').type('establecimientoPrueba')
-    cy.get('#password').type('establecimientoPrueba1')
+//     cy.get('#email').type('establecimientoPrueba@gmail.com')
+//     cy.get('#phone').type('666666666')
+//     cy.get('#username').type('establecimientoPrueba')
+//     cy.get('#password').type('establecimientoPrueba1')
 
 
-    //  ? franja elegir hora
-    cy.get('#franjaTiempo').type('30');
-    cy.get('#numberOfPeople').type('4')
+//     //  ? franja elegir hora
+//     cy.get('#franjaTiempo').type('30');
+//     cy.get('#numberOfPeople').type('4')
    
-    cy.get('#enviarRegistroEstablecimiento').click()
+//     cy.get('#enviarRegistroEstablecimiento').click()
 
-  })
-})
+//   })
+// })
--- client/sientate-a-la-mesa/cypress/e2e/registroUsuario.cy.ts
+++ client/sientate-a-la-mesa/cypress/e2e/registroUsuario.cy.ts
@@ -1,31 +1,31 @@
-import axios from 'axios';
-import { baseUrl } from '../../src/env/env-variables';
+// import axios from 'axios';
+// import { baseUrl } from '../../src/env/env-variables';
 
-describe('Registro de usuario', () => {
-  beforeEach(async () => {
+// describe('Registro de usuario', () => {
+//   beforeEach(async () => {
     
-    const userDelete = {
-      userName: "peter",
-    }
+//     const userDelete = {
+//       userName: "peter",
+//     }
     
-    const response = await axios.delete(`${baseUrl}users?userName=${userDelete.userName}`);    
+//     const response = await axios.delete(`${baseUrl}users?userName=${userDelete.userName}`);    
     
 
-  })
+//   })
 
-  it('Registro de usuario', () => {
+//   it('Registro de usuario', () => {
     
-    cy.visit('http://localhost:5173')
-    cy.get('#botonRegistro').click()
-    cy.get('#botonRegistroUsuario').click()
-    cy.get('#firstname').type('Peter')
-    cy.get('#lastname').type('parker')
-    cy.get('#email').type('alu@gmail.com')
-    cy.get('#phone').type('999887766')
-    cy.get('#username').type('peter')
-    cy.get('#password').type('peter200A')
-    cy.get('#address').type('Calle falsa 123')
-    cy.get('#enviarRegistroUsuario').click()
+//     cy.visit('http://localhost:5173')
+//     cy.get('#botonRegistro').click()
+//     cy.get('#botonRegistroUsuario').click()
+//     cy.get('#firstname').type('Peter')
+//     cy.get('#lastname').type('parker')
+//     cy.get('#email').type('alu@gmail.com')
+//     cy.get('#phone').type('999887766')
+//     cy.get('#username').type('peter')
+//     cy.get('#password').type('peter200A')
+//     cy.get('#address').type('Calle falsa 123')
+//     cy.get('#enviarRegistroUsuario').click()
 
-  })
-})
+//   })
+// })
