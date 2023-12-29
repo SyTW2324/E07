00b3a5bea498f6f19743b07aca55278ecf646a9f
Ismael Martín Herrera <alu0101397375@ull.edu.es>
2023-12-29 11:16:11 +0000

    prueba despliegue cyclic

--- client/sientate-a-la-mesa/src/env/env-variables.ts
+++ client/sientate-a-la-mesa/src/env/env-variables.ts
@@ -4,4 +4,4 @@
 // http://localhost:8081/
 // https://long-lamb-kerchief.cyclic.app
 
-export const baseUrl: string = "https://long-lamb-kerchief.cyclic.app"
+export const baseUrl: string = "https://kind-ruby-worm-fez.cyclic.app"
