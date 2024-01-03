e51a27dbc0e714edecda85b7f460395239206fb0
Marco Barroso <alu0101386560@ull.edu.es>
2024-01-03 12:42:44 +0000

    mensajes de depuración eliminados

--- server/src/models/restaurants/restaurants.ts
+++ server/src/models/restaurants/restaurants.ts
@@ -3,22 +3,16 @@ import { reservationModel } from "../reservations/reservantions-models.js";
 
 export async function addHistoricReservations(userName: string): Promise<boolean> {
   try {
-
-    console.log("addHistoricReservations");
     const user = await RestaurantModel.findOne({ userName: userName });
     if (user === null) return false;
     const nextReservations = user.nextReservations;
     for (let i = 0; i < nextReservations.length; i++) {
       const reservationId = nextReservations[i];
       const reservation = await reservationModel.findById(reservationId);
-      console.log("reservation ", reservation);
       if (reservation === null) return false;
       const reservationDate = new Date(reservation.day);
       const currentDate = new Date();
-      console.log("reservation date ", reservationDate);
-      console.log("current date ", currentDate);
       if (reservationDate < currentDate) {
-        console.log("actualizando reserva")
         await RestaurantModel.findOneAndUpdate({ userName: userName }, {
           $push: { historicReservations: reservationId }
         });
