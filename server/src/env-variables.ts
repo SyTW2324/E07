c351d3a9fa7a3e143430cba60fbd05bfeabebac5
Marco Barroso <alu0101386560@ull.edu.es>
2024-01-09 11:04:20 +0000

    cambio en la uri de las bases de datos

--- server/src/env-variables.ts
+++ server/src/env-variables.ts
@@ -2,13 +2,13 @@
  * En este fichero se especifican todas las variables de entorno requeridas por la aplicación.
  */
 
-export const uri_bbdd: string = 'mongodb+srv://adminProyecto:XrGAiCeGbROwlsPv@cluster0.opw3djq.mongodb.net/sientatealamesa2';
+export const uri_bbdd: string = 'mongodb+srv://adminProyecto:XrGAiCeGbROwlsPv@cluster0.opw3djq.mongodb.net/sientatealamesa';
 
 export const uri_bbdd_test: string = 'mongodb+srv://adminProyecto:XrGAiCeGbROwlsPv@cluster0.opw3djq.mongodb.net/sientatealamesa-test';
 
 export const uri_bbdd_local: string = 'mongodb://adminProyecto:1234@172.16.131.2:27017/sientateMesa?retryWrites=true&w=majority';
 
-export const uri_bbdd_dev: string = 'mongodb+srv://adminProyecto:XrGAiCeGbROwlsPv@cluster0.opw3djq.mongodb.net/sientatealamesa-dev2';
+export const uri_bbdd_dev: string = 'mongodb+srv://adminProyecto:XrGAiCeGbROwlsPv@cluster0.opw3djq.mongodb.net/sientatealamesa-dev';
 
 export const port: number = 8081;
 
